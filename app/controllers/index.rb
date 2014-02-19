ae5c50bc5b244a0e00e6bd5fcb21815a2f48777e
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 12:27:39 -0600

    fixed some bugs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,23 +4,34 @@ get '/' do
 end
 
 post '/generate' do
-  if (a = Url.find_by(long_url: params["long_url"]))
-    @short_url = a.short_url
+  if params["long_url"].match(URI.regexp)
+    if (a = Url.find_by(long_url: params["long_url"]))
+      puts "found---------------------------------"
+      @short_url = a.short_url
+    else
+      puts "not found. Create---------------------------------"
+      p a = Url.create(long_url: params["long_url"])
+      puts " created -----------------------------"
+      @short_url = a.short_url
+    end
+
+    erb :shorty
   else
-   a = Url.create(long_url: params["long_url"])
-    @short_url = a.short_url
+    @message = "URL invalid or not found"
+    erb :index
   end
-  erb :shorty
 end
 
 get '/:short_url' do
   @shorty = Url.find_by(short_url: params[:short_url])
   if @shorty
-    erb :get_shorty
+    @shorty.count += 1
+    @shorty.save
+    if @shorty.long_url.match(URI.regexp)
+      redirect(@shorty.long_url)
+    end
   else
-    #display not found
-
-    @message = "URL not found"
+    @message = "URL not found or invalid"
     erb :index
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,8 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
-  before_save  :generate_short_url
+  before_create  :generate_short_url
   validates :short_url, uniqueness: true
-  validates :long_url, uniqueness: true
+  validates :long_url, uniqueness: true , presence: true
   def generate_short_url
     self.short_url = SecureRandom.base64(6).gsub(/\W/,"")
   end
